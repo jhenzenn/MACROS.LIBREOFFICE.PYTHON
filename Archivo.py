0a18dd766e12c4daa1436a24068882f96fdc6055
Juan P. Henzenn <jhenzenn@gmail.com>
2017-07-20 12:12:14 -0300

    Initial commit * Funciona crear una sesion de Libreoffice sobre la cual trabajar. * Funciona cargar un csv desde archivo.

---
+++ Archivo.py
@@ -0,0 +1,54 @@
+import os
+import uno
+import time
+
+def InicializarLODesktop(nIntentos):
+    local = uno.getComponentContext()
+    resolver = local.ServiceManager.createInstanceWithContext("com.sun.star.bridge.UnoUrlResolver", local)
+
+    for i in range(1,nIntentos):
+        try:
+            time.sleep(1)
+            context = resolver.resolve("uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
+            if context:
+                oDesktop = context.ServiceManager.createInstanceWithContext("com.sun.star.frame.Desktop", context)
+            return oDesktop
+            break
+        except:
+            print("No puedo inicializar - Intento: " + i)
+
+def importCSV(sCSVFileURL, sFrame, bVisible):
+    from com.sun.star.beans import PropertyValue
+
+    tDataProperties = []
+    p = PropertyValue()
+
+    p.value.Name = "Hidden"
+    p.value.Value = not bVisible
+    tDataProperties.append(p)
+
+    p.value.Name = "FilterName"
+    p.value.Value = "Text - txt - csv (StarCalc)"
+    tDataProperties.append(p)
+
+    p.value.Name = "FilterOptions"
+    p.value.Value = "59,34,IBMPC_850,1,,0,false,true"
+    tDataProperties.append(p)
+
+    oDesktop = InicializarLODesktop(6)
+    oDataDoc = oDesktop.loadComponentFromURL(sCSVFileURL, sFrame, 0, tDataProperties)
+    if not oDataDoc:
+        #print("Fallo al abrir archivo")
+        return None
+    else:
+        return oDataDoc
+
+'''def Main():
+    oDoc = importCSV("file:///media/linux/csv/E01/resumen1.csv","_blank",True)
+    if not oDoc:
+        print("Fallo al abrir el archivo")
+    else:
+        oDoc.dispose()
+        quit()
+
+Main()'''
---
+++ hola.py
@@ -0,0 +1,29 @@
+import os
+import time
+import sys
+import logging
+import uno
+import Archivo
+
+'''logging.basicConfig()
+log = logging.getLogger("Holapp")
+log.setLevel(logging.DEBUG) #set verbosity to show all messages of severity >= DEBUG
+log.info("Starting my app")'''
+
+librebin = os.popen('which libreoffice | tr -d \'\\n\'').read()
+libre_param = "--accept=\"socket,host=localhost,port=2002;urp;\" --norestore &"
+
+os.system(librebin + " " + libre_param)
+#os.system('/opt/openoffice4/program/soffice -accept=\"socket,host=localhost,port=2002\;urp\;\" &')
+#time.sleep(6)
+
+oDoc = Archivo.importCSV("file:///media/linux/csv/E01/1888.csv","_blank",True)
+if not oDoc:
+    print("Fallo al abrir el archivo")
+else:
+    #time.sleep(10)
+    oDoc.dispose()
+
+quit()
+
+
